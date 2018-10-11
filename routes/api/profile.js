e2ad38d31fad99bdcab8c72fbccdfca225318be6
El-Ramahi <Samer.El-Ramahi@rjwgroup.com>
2018-10-11 09:32:31 -0500

    add two get routes for profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -40,6 +40,43 @@ router.get(
       .catch(err => res.status(404).json("errors"));
   }
 );
+//@route    Get api/profile/handle/:handle
+//@desc     Get profile by handle
+//@access   Public
+
+router.get('/handle/:handle', (req, res) => {
+  const errors = {}
+  Profile.findOne({ handle: req.params.handle })
+  .populate("user", ["name", "avatar"])
+  .then(profile => {
+    if(!profile) {
+      errors.noprofile = 'There is no profile for this user';
+      res.status(404).json(errors);
+    }
+    
+    res.json(profile);
+  })
+  .catch(err => res.status(404).json(err))
+})
+
+//@route    Get api/profile/user/:user_id
+//@desc     Get profile by handle
+//@access   Public
+
+router.get('/user/:user_id', (req, res) => {
+  const errors = {}
+  Profile.findOne({ user: req.params.user_id })
+  .populate("user", ["name", "avatar"])
+  .then(profile => {
+    if(!profile) {
+      errors.noprofile = 'There is no profile for this user';
+      res.status(404).json(errors);
+    }
+    
+    res.json(profile);
+  })
+  .catch(err => res.status(404).json({profile: 'There is no profile for this user'}))
+})
 //@route    POST api/profile
 //@desc     Create or edit user profile
 //@access   Private
