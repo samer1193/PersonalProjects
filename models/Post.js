7a23da07759b3f690b0f92a952297ece27d78d69
El-Ramahi <Samer.El-Ramahi@rjwgroup.com>
2018-10-12 15:22:16 -0500

    add post routes

--- models/Post.js
+++ models/Post.js
@@ -2,4 +2,41 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 //Create schema
-const PostSchema = 
+const PostSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String
+    },
+    avatar: {
+        type: String
+    },
+    likes: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            },
+        text: {
+            type: String,
+            required: true
+        },
+        date: {
+            type: Date,
+            default: Date.now
+        }
+        }   
+    ],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = Post = mongoose.model('post', PostSchema)
